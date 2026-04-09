6c38534bd660a0751f40339ea4fe9a14a58c707a
dusdn5009-max <dusdn5009@gmail.com>
2026-04-09 17:26:35 +0900

    Add additional images to service worker cache

--- sw.js
+++ sw.js
@@ -9,7 +9,17 @@ const ASSETS = [
   "./images/image2.jpg",
   "./images/image3.jpg",
   "./images/image4.jpg",
-  "./images/image5.jpg"
+  "./images/image5.jpg",
+  "./images/image6.jpg",
+  "./images/image7.jpg",
+  "./images/image8.jpg",
+  "./images/image9.jpg",
+  "./images/image10.jpg",
+  "./images/image11.jpg",
+  "./images/image12.jpg",
+  "./images/image13.jpg",
+  "./images/image14.jpg",
+  "./images/image15.jpg"
 ];
 
 self.addEventListener("install", (event) => {
